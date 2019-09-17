eac335a146d04d2c9a1660ad7184e6d3414c56b1
Ihzanul <shuo5789@gmail.com>
2019-09-17 12:01:27 +0800

    third commit

--- controllers/mahasiswaControllers.js
+++ controllers/mahasiswaControllers.js
@@ -1,21 +1,42 @@
 const express = require('express');
 var router = express.Router();
 const mongoose = require('mongoose');
-const Mahasiswa = mongoose.model('Mahasiswa');
-
-var Mahasiswas = new Mahasiswa();
+const MahasiswaModel = mongoose.model('mahasiswa');
+const MailsModel = mongoose.model('mails');
 
 router.get('/', (req, res) => {
-    Mahasiswa.find((err, docs) => {
+    // var Mahasiswa = new MahasiswaModel();
+    // Mahasiswa.No = 1;
+    // Mahasiswa.namaMahasiswa = "Ihznaul"
+    // Mahasiswa.save();
+
+    MailsModel.find((err, resbak) => {
         if (!err) {
+                MahasiswaModel.find(null, { '_id': false, 'nim': true}, function(err, resdikti) {
+                    if (err) {
+                        console.log('Error during find mails ' + err);
+                    } else {
+                        for (var i = 0; i < resdikti.length; i++) {
+                            MailsModel.findOne({'nim': resdikti[i].nim}, function(err, rescur) {
+                                if (err) {
+                                    console.log('error during matching ' + err)
+                                } else if (rescur == null){
+                                    console.log(`tidak sama ${i}`)
+                                } else {
+                                    console.log(`stambuk bak ${rescur.nim} sama dengan dikti ${resdikti.nim} pada index ${i}`)
+                                }
+                            })
+                        }
+                    }
+                }).limit(11);
             res.render("kkn/list", {
-                daftar: docs
+                daftar: resbak
             });
-            console.log(docs.length)
+            // console.log(docs[1].nim);
         } else {
             console.log('Error in retriving list : ' + err);
         }
-    });
+    }).limit(20)
 });
 
 module.exports = router;
--- models/mahasiswa.model.js
+++ models/mahasiswa.model.js
@@ -19,21 +19,28 @@ var mahasiswaSchema = new monggose.Schema({
     statusMahasiswa: {
         type: String
     }
-
-    // mailNumber: {
-    //     type: Number
-    // },
-    // nama: {
-    //     type: String,
-    //     required: 'This field is required.'
-    // },
-    // stambuk: {
-    //     type: String,
-    //     required: 'This field is required'
-    // },
-    // sign: {
-    //     type: String
-    // }
 });
 
-monggose.model('Mahasiswa', mahasiswaSchema);
+var mailsSchema = new monggose.Schema({
+    no: {
+        type: Number
+    },
+    nim: {
+        type: String
+    },
+    namaMahasiswa: {
+        type: String
+    },
+    prodi: {
+        type: String
+    },
+    jenjang: {
+        type: String
+    },
+    statusMahasiswa: {
+        type: String
+    }
+});
+
+monggose.model('mahasiswa', mahasiswaSchema);
+monggose.model('mails', mailsSchema);
